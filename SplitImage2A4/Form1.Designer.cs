2ed9fd6dcd01cf8aca243bd173f6c127a7d660eb
bitzhuwei <bitzhuwei.cnblogs@qq.com>
2020-02-22 22:26:51 +0800

    添加项目文件。

---
+++ SplitImage2A4/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SplitImage2A4
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.btnOpen = new System.Windows.Forms.Button();
+			this.imgSource = new System.Windows.Forms.PictureBox();
+			this.txtImageInfo = new System.Windows.Forms.TextBox();
+			this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+			this.btnSplit = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.imgSource)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnOpen
+			// 
+			this.btnOpen.Location = new System.Drawing.Point(12, 12);
+			this.btnOpen.Name = "btnOpen";
+			this.btnOpen.Size = new System.Drawing.Size(75, 23);
+			this.btnOpen.TabIndex = 0;
+			this.btnOpen.Text = "Open...";
+			this.btnOpen.UseVisualStyleBackColor = true;
+			this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+			// 
+			// imgSource
+			// 
+			this.imgSource.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.imgSource.Location = new System.Drawing.Point(12, 70);
+			this.imgSource.Name = "imgSource";
+			this.imgSource.Size = new System.Drawing.Size(776, 368);
+			this.imgSource.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.imgSource.TabIndex = 1;
+			this.imgSource.TabStop = false;
+			// 
+			// txtImageInfo
+			// 
+			this.txtImageInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+			this.txtImageInfo.Font = new System.Drawing.Font("宋体", 16F);
+			this.txtImageInfo.Location = new System.Drawing.Point(93, 12);
+			this.txtImageInfo.Multiline = true;
+			this.txtImageInfo.Name = "txtImageInfo";
+			this.txtImageInfo.ReadOnly = true;
+			this.txtImageInfo.Size = new System.Drawing.Size(695, 52);
+			this.txtImageInfo.TabIndex = 2;
+			// 
+			// dlgOpenFile
+			// 
+			this.dlgOpenFile.Filter = "*.*|*.*";
+			// 
+			// btnSplit
+			// 
+			this.btnSplit.Location = new System.Drawing.Point(12, 41);
+			this.btnSplit.Name = "btnSplit";
+			this.btnSplit.Size = new System.Drawing.Size(75, 23);
+			this.btnSplit.TabIndex = 0;
+			this.btnSplit.Text = "Split";
+			this.btnSplit.UseVisualStyleBackColor = true;
+			this.btnSplit.Click += new System.EventHandler(this.btnSplit_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.txtImageInfo);
+			this.Controls.Add(this.imgSource);
+			this.Controls.Add(this.btnSplit);
+			this.Controls.Add(this.btnOpen);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "Form1";
+			this.Text = "Split Image to A4 size.";
+			((System.ComponentModel.ISupportInitialize)(this.imgSource)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOpen;
+		private System.Windows.Forms.PictureBox imgSource;
+		private System.Windows.Forms.TextBox txtImageInfo;
+		private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+		private System.Windows.Forms.Button btnSplit;
+	}
+}
+
---
+++ SplitImage2A4/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SplitImage2A4
+{
+	public partial class Form1 : Form
+	{
+		Bitmap sourceImage;
+		string filename;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnOpen_Click(object sender, EventArgs e)
+		{
+			if (this.dlgOpenFile.ShowDialog() == DialogResult.OK)
+			{
+				string filename = this.dlgOpenFile.FileName;
+				try
+				{
+					this.sourceImage = new Bitmap(filename);
+					this.filename = (new FileInfo(filename)).FullName;
+					this.imgSource.Image = this.sourceImage;
+					this.imgSource.SizeMode = PictureBoxSizeMode.Zoom;
+					float xDpi = this.sourceImage.HorizontalResolution;
+					float yDpi = this.sourceImage.VerticalResolution;
+					float width = this.sourceImage.Width;
+					float height = this.sourceImage.Height;
+					// A4: 8.267 x 11.692 inches
+					float hA4 = yDpi * 8.267f;
+					float vA4 = xDpi * 11.692f;
+					int pH =(int) Math.Ceiling( width / hA4);
+					int pV =(int)Math.Ceiling( height / vA4);
+					var builder = new StringBuilder();
+					//builder.AppendFormat($"fullname:{this.filename}");
+					//builder.AppendLine();
+					builder.AppendFormat($"DPI:{xDpi}, {yDpi}");
+					//builder.AppendLine();
+					builder.AppendFormat($", Size:{width}, {height}");
+					builder.AppendLine();
+					builder.AppendFormat($"=>:{pH}*{pV} parts.");
+					builder.AppendLine();
+					this.txtImageInfo.Text = builder.ToString();
+				}
+				catch (Exception ex)
+				{
+					MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+			}
+		}
+
+		private void btnSplit_Click(object sender, EventArgs e)
+		{
+			if (this.sourceImage == null)
+			{
+				MessageBox.Show("Please Select an image first!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
+
+			this.btnSplit.Enabled = false;
+			
+			float xDpi = this.sourceImage.HorizontalResolution;
+			float yDpi = this.sourceImage.VerticalResolution;
+			float width = this.sourceImage.Width;
+			float height = this.sourceImage.Height;
+
+			// A4: 8.267 x 11.692 inches
+			float hA4 = yDpi * 8.267f;
+			float vA4 = xDpi * 11.692f;
+			float cursorX = 0;
+			float cursorY = 0;
+			int indexX = 0;
+			int indexY = 0;
+			while (cursorY <= height)
+			{
+				while (cursorX <= width)
+				{
+					Bitmap bmpPart = null;
+					float pW = 0; float pH = 0;
+					//if (cursorX + hA4 <= width) { pW = hA4; }
+					//else { pW = width - cursorX; }
+					//if (cursorY + vA4 <= height) { pH = vA4; }
+					//else { pH = height - cursorY; }
+					//bmpPart = new Bitmap((int)pW, (int)pH);
+					bmpPart = new Bitmap((int)hA4, (int)vA4);
+					bmpPart.SetResolution(xDpi, yDpi);
+					using (var g = Graphics.FromImage(bmpPart))
+					{
+						g.DrawImage(this.sourceImage,
+							new RectangleF(0, 0, hA4, vA4),
+							new RectangleF(cursorX, cursorY, hA4, vA4),
+							 GraphicsUnit.Pixel);
+					}
+					bmpPart.Save(string.Format($"{this.filename}_{indexY:00}_{indexX:00}.png"));
+					bmpPart.Dispose();
+					indexX++;
+					cursorX = cursorX + hA4;
+				}
+				indexY++;
+				cursorX = 0;
+				cursorY = cursorY + vA4;
+			}
+
+			this.btnSplit.Enabled = true;
+		}
+	}
+}
